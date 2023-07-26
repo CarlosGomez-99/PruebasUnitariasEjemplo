3d4af1fbf62a892883b3c7fed5324e56663fc4df
Carlos Gómez <drcgomez@hotmail.com>
2023-07-26 10:43:58 -0500

    Cache clean

--- PruebasUnitariasXUnit/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using PruebasUnitariasXUnit;
-
-Console.WriteLine("Numero 1:");
-int num1 = int.Parse(Console.ReadLine());
-Console.WriteLine("Numero 2:");
-int num2 = int.Parse(Console.ReadLine());
-var calculadora = new Calculadora();
-Console.WriteLine($"Suma: {calculadora.Suma(num1, num2)}");
