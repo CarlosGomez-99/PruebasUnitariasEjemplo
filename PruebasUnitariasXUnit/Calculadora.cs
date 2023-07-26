96a1b7f27b6251250e5b7aa7b5d56561aeb1f5ee
Carlos Gómez <drcgomez@hotmail.com>
2023-07-26 10:38:48 -0500

    Proyecto de ejemplo pruebas unitarias

---
+++ PruebasUnitariasXUnit/Calculadora.cs
@@ -0,0 +1,16 @@
+﻿namespace PruebasUnitariasXUnit;
+public class Calculadora
+{
+    public int Suma(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+    public int Division(int num1, int num2)
+    {
+        if (num2 == 0)
+        {
+            throw new Exception("No se puede dividir entre cero");
+        }
+        return num1 / num2;
+    }
+}
